fc25d53d18101805db9c06ca217470442adea97c
Kelly Miranda <kellymiranda@wposs.com>
2021-12-09 21:35:50 -0500

    ADD: Commit inicial

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+
+
+function NavBar(){
+    return (
+          <nav >
+              <ul>
+                  <li><a href="#" title="Enlace genérico">Incio</a></li>
+                  <li><a href="#" title="Enlace genérico">Usuario</a></li>
+                  <li><a href="#" title="Enlace genérico">Preguntas</a></li>
+              </ul>
+          </nav>
+    )
+}
+
+export {NavBar}
