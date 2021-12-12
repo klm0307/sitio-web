ff45d6ae9ca365e980b43226327d0af167e5ee41
Kelly Miranda <kellymiranda@wposs.com>
2021-12-11 22:43:25 -0500

    🛠️ FIX: correccion de questions container

--- src/pages/Question/container/CardContainer/index.js
+++ src/pages/Question/container/CardContainer/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardQuestionTitle } from "../../components/Card/CardQuestionTitle";
 import { CardQuestionText } from "../../components/Card/CardQuestionText";
 import { CardQuestionImage } from "../../components/Card/CardQuestionImg";
+import { listQuestions } from "../../services/questionService";
 import "./style.css";
 
 const CardContainer = () => {
+  const questions = listQuestions();
+  const [questionSelected, setQuestionSelected] = useState(questions[0]);
+
+  const setQuestion = (index) => {
+    setQuestionSelected(questions[index]);
+  };
+
   return (
     <section>
       <div className="list-card-question">
@@ -13,13 +21,28 @@ const CardContainer = () => {
         </div>
         <div className="question">
           <div className="title-question">
-            <CardQuestionTitle title={"A"} />
+            <CardQuestionTitle title={questionSelected.title} />
           </div>
           <div className="text-question">
-            <CardQuestionText text={"B"} />
+            <CardQuestionText text={questionSelected.text} />
           </div>
           <div className="navigate">
-            <ul className="navigate-question"></ul>
+            <ul className="navigate-question">
+              {questions.map((question, index) => {
+                return (
+                  <li key={index}>
+                    <button
+                      className={
+                        questionSelected.id === question.id
+                          ? "active-question"
+                          : ""
+                      }
+                      onClick={() => setQuestion(index)}
+                    ></button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
